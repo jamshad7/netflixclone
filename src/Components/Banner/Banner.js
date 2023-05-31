6be6c7fdcba661406acca3f5ad44cdd5878ce84d
jamshad7 <jaffaralijaffar229@gmail.com>
2023-05-31 10:13:30 +0530

    my commit

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -32,7 +32,7 @@ function Banner() {
       clearInterval(intervalRef.current);
     };
   }, [movies]);
-
+ 
   const currentMovie = movies[currentMovieIndex];
 
   return (
@@ -61,7 +61,7 @@ function Banner() {
           <Link to={currentMovie ? `/video/${currentMovie.id}` : null}>
             <button className="button">Play</button>
           </Link>
-          <button className="button">My List</button>
+          <button className="button">My Listss</button>
         </div>
       </div>
       <div className="fade_bottom"></div>
